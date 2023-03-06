8b7217010c1907988490efe30f4e2b0dfd08112f
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-07 00:45:27 +0100

    début page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,6 +9,20 @@ export default {
 
 <template>
 <Header />
+
+<div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-5  ">
+
+<div>
+      <section class="md:h-screen h-fit bg-white">
+         <div class="grid ">
+          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase">Fabio</div>
+          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase">Gafforelli</div>
+
+        </div>
+
+      </section>
+      </div>
+      </div>
 </template>
 
 <style>

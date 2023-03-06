15e3ac1c98bd574f10353eee940e8b32f0a75f8f
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-07 00:28:35 +0100

    import de firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,40 @@ import App from './App.vue'
 import router from './router'
 
 import './index.css'
+import mitt from 'mitt';
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";  // Initialize Firebase
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyD1wdglbT6plqfHjUbvg_cuxgJbodWM7X4",
+  authDomain: "portfolio-fb0f2.firebaseapp.com",
+  projectId: "portfolio-fb0f2",
+  storageBucket: "portfolio-fb0f2.appspot.com",
+  messagingSenderId: "359610302999",
+  appId: "1:359610302999:web:71411bec87f1c63583d252"
+};
+
+
+// Initialize Firebase
+
+const appFirebase = initializeApp(firebaseConfig);
+
 
 const app = createApp(App)
+
+
+export const emitter = mitt();
+app.config.globalProperties.emitter = emitter;
+
+
 
 app.use(router)
 

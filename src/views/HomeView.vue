1c3c0998bcf71d9500c81f7df8118a8131a53d4e
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-07 01:22:16 +0100

    Ajout du typewritter page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,61 @@
 <script>
 import Header from '../components/layouts/header.vue';
 export default {
+  data: () => {
+    return {
+      typeValue: '',
+      typeStatus: false,
+      typeArray: ['Neon', 'Chamber', 'Bronze', 'KJ Lineup'],
+      typingSpeed: 200,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      typeArrayIndex: 0,
+      charIndex: 0
+    }
+  },
+  methods: {
+    typeText(){
+      if(this.charIndex < this.typeArray[this.typeArrayIndex].length) {
+        if(!this.typeStatus)
+        this.typeStatus = true;
+
+        this.typeValue += this.typeArray[this.typeArrayIndex].charAt(this.charIndex);
+        this.charIndex += 1;
+
+        setTimeout(this.typeText, this.typingSpeed);
+      }
+      else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if(this.charIndex > 0) {
+        if(!this.typeStatus)
+        this.typeStatus = true;
+
+        this.typeValue = this.typeArray[this.typeArrayIndex].substring(0, this.charIndex - 1);
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed); 
+      }
+      else {
+        this.typeStatus = false;
+        this.typeArrayIndex += 1;
+        if(this.typeArrayIndex >= this.typeArray.length)
+        this.typeArrayIndex = 0;
+
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    }
+  },
+  created(){
+    setTimeout(this.typeText, this.newTextDelay + 200);
+  },
     name:"App",
     components: { Header },
 }
 
 </script>
-
 <template>
 <Header />
 
@@ -15,8 +64,13 @@ export default {
 <div>
       <section class="md:h-screen h-fit bg-white">
          <div class="grid ">
-          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase">Fabio</div>
-          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase">Gafforelli</div>
+          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase"><h1>Fabio</h1></div>
+          <div class="col-span-2 md:text-[190px] text-center text-6xl text-[#252C35] uppercase"><h1>Gafforelli</h1></div>
+          <div class="col-span-2 md:text-9xl text-center text-2xl text-green-600 ">
+            <span class="typed-text">{{ typeValue }}</span>
+            <span class="cursor" :class="{'typing' : typeStatus}">&nbsp;</span>
+          </div>
+          
 
         </div>
 
@@ -25,6 +79,23 @@ export default {
       </div>
 </template>
 
-<style>
-
+<style scoped>
+  span.typed-text {
+    color: #D2B94B;
+  }
+  span.cursor {
+    display: inline-block;
+    margin-left: 3px;
+    width: 4px;
+    background-color: #000;
+    animation: cursorBlink 1s infinite;
+  }
+  span.cursor.typing {
+    animation: none
+  }
+  @keyframes cursorBlink {
+    49% {background-color: #000 ;}
+    50% {background-color: transparent ;}
+    99% {background-color: transparent ;}
+  }
 </style>

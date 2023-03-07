d3c11958dba227a3860ad6f2d3d6c1bd9fd1a736
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-07 12:19:52 +0100

    router mis a jour

--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,26 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AdminView from '../views/AdminView.vue';
 import View404 from '../views/404View.vue';
-import ProjetsView from '../views/ProjetsView.vue';
 
-
+import CreateProjets from '../views/projets/CreateView.vue';
+import UpdateProjets from '../views/projets/UpdateView.vue';
+import DeleteProjets from '../views/projets/DeleteView.vue';
+import ListeProjets from '../views/projets/ListeView.vue';
+import idprojet from  '../views/projets/[id].vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', name: 'Home', component: HomeView },
     { path: '/admin', name: 'AdminView', component: AdminView },
-    { path: '/projets', name: 'ProjetsView', component: ProjetsView },
 
     { path: '/:pathMatch(.*)*', name: 'View404', component: View404 },
 
+    { path: '/projets', name: 'ListeProjets', component: ListeProjets },
+    { path: '/projet/:id', name: 'idprojet', component: idprojet },
+    { path: '/createProjet', name: 'CreateProjets', component: CreateProjets  },
+    { path: '/updateProjets/:id', name: 'UpdateProjets', component: UpdateProjets },
+    { path: '/deleteProjets/:id', name: 'DeleteProjets', component: DeleteProjets },
 
 
   ]

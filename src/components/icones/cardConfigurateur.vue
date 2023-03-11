9d176bbc3f0f22aca85508ae473e4ff74d699df0
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-11 01:04:48 +0100

    targe blank pdf

--- src/components/icones/cardConfigurateur.vue
+++ src/components/icones/cardConfigurateur.vue
@@ -22,7 +22,7 @@
         class="inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-black "
         data-te-ripple-init
         data-te-ripple-color="light">
-        <a href="https://docs.google.com/presentation/d/e/2PACX-1vSMGax1cjDp1ystoKPwPvVgp84XToij1xQX3JqUywmBowONIy0tDOsgDWuauWcJNFAHvmTLA19vi00L/pub?start=false&loop=false&delayms=3000">
+        <a href="https://docs.google.com/presentation/d/e/2PACX-1vSMGax1cjDp1ystoKPwPvVgp84XToij1xQX3JqUywmBowONIy0tDOsgDWuauWcJNFAHvmTLA19vi00L/pub?start=false&loop=false&delayms=3000" target="_blank">
         Voir le cours
         </a>
       </button>

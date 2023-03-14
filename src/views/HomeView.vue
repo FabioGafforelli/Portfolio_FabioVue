8afd62e8d7bce503fd5478f005990f05025a215b
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-14 19:57:20 +0100

    Retrait lettre compétence

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -141,26 +141,7 @@ export default {
 
       </section>
 
-<section>
 
-<div class="mi-outer">
-  <div class="mi">
-    <div class="mi-flap2"></div>
-    <label for="moreInfo" class="mi-flap1"></label>
-    <div class="mi-flap5">
-      <div class="mi-info"></div>
-    </div>
-    <div class="mi-flap4">
-      <div class="mi-info">
-        <label for="moreInfo" class="mi-close">X</label>
-      </div>
-    </div>
-    <div class="mi-flap3">
-      <div class="mi-info"></div>
-    </div>
-  </div>
-</div>
-      </section>
       </div>
       </div>
     </div>

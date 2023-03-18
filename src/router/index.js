3ecbf33cad2e0357cfa41456a8b55f4a66c9d13e
FabioGafforelli <fabio.gafforelli@edu.univ-fcomte.fr>
2023-03-18 11:42:47 +0100

    page contact

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import View404 from '../views/404View.vue';
+import ContactView from '../views/ContactView.vue';
 
 import ListeProjets from '../views/projets/ListeView.vue';
 import CoursView from '../views/projets/CoursView.vue';
@@ -12,6 +13,9 @@ const router = createRouter({
 
     { path: '/:pathMatch(.*)*', name: 'View404', component: View404 },
 
+    { path: '/contact', name: 'ContactView', component: ContactView },
+   
+   
     { path: '/projets', name: 'ListeProjets', component: ListeProjets },
     { path: '/cours', name: 'CoursView', component: CoursView },
 
---
+++ src/views/ContactView.vue
@@ -0,0 +1,64 @@
+<script>
+import Header from '../components/layouts/header.vue';
+
+export default {
+    name:"App",
+    components: { Header },
+    data() {
+    return {
+      from_name: "",
+      from_firstname: "",
+      email_id: "",
+      message: "",
+    };
+  },
+  methods: {
+    sendMail() {
+      const params = {
+        from_name: this.from_name,
+        from_firstname: this.from_firstname,
+        email_id: this.email_id,
+        message: this.message,
+      };
+      emailjs
+      .send("service_430dq2r", "template_15d9h0l", params)
+        .then(res => {
+          alert(`Success! ${res.status}`);
+        })
+        .catch(err => {
+          alert(`Error: ${err}`);
+        });
+    },
+  },
+};
+</script>
+
+<template>
+    <div class="bg-gray-50">
+            <Header />
+        <div class="pt-5 px-5 gap-20 relative">
+            <div class="bg-gray-100 p-4 rounded-lg">
+    <form class="space-y-4" @submit.prevent="submitForm">
+      <div>
+        <label for="name" class="block text-gray-700 font-bold mb-2">Name</label>
+        <input type="text" id="name" v-model="from_name" placeholder="Your name"
+          class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+      </div>
+      <div>
+        <label for="email" class="block text-gray-700 font-bold mb-2">Email</label>
+        <input type="email" id="email" v-model="email_id" placeholder="Your email"
+          class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500">
+      </div>
+      <div>
+        <label for="message" class="block text-gray-700 font-bold mb-2">Message</label>
+        <textarea id="message" v-model="message" placeholder="Your message"
+          class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:border-blue-500"></textarea>
+      </div>
+      <div>
+        <button type="submit" @click.prevent="sendMail" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Submit</button>
+      </div>
+    </form>
+  </div>
+        </div>
+        </div>
+</template>
